ac8e40e747f61539e70e90b9276063028915e8ef
Ferrakkem Bhuiyan <ferrakkem@gmail.com>
2020-09-13 17:42:25 -0400

    Contacts cell added.

---
+++ My Contacts/Views/ContactcTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ContactcTableViewCell.swift
+//  My Contacts
+//
+//  Created by Ferrakkem Bhuiyan on 2020-09-13.
+//  Copyright © 2020 Ferrakkem Bhuiyan. All rights reserved.
+//
+
+import UIKit
+
+class ContactcTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
