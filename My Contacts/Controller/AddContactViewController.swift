df0dc1441b55b08a4d2e1b574875db7a8bd70db3
Ferrakkem Bhuiyan <ferrakkem@gmail.com>
2020-09-14 17:06:23 -0400

    Add and Contacts Details View updated

--- My Contacts/Controller/AddContactViewController.swift
+++ My Contacts/Controller/AddContactViewController.swift
@@ -9,20 +9,20 @@
 import UIKit
 
 class AddContactViewController: UIViewController {
-
+    
+    @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var firstName: UITextField!
+    
+    @IBOutlet weak var companyName: UITextField!
+    @IBOutlet weak var lastName: UITextField!
+    @IBOutlet weak var emailAddress: UITextField!
+    @IBOutlet weak var number: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-
-    /*
-    // MARK: - Navigation
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
+
+
